f61080a5acdece49c0c6f0c3d8d4675619745afa
CESAR Djelissa <djelissacesar653@gmail.com>
2025-09-19 17:06:15 -0400

    first commit

---
+++ app/src/main/java/com/example/flashcard/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.flashcard
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.View
+import android.widget.TextView
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        flashcardQuestion.setOnClickListener {
+            flashcardQuestion.visibility = View.INVISIBLE
+            flashcardAnswer.visibility = View.VISIBLE
+        }
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility = View.VISIBLE
+            flashcardAnswer.visibility = View.INVISIBLE
+        }
+    }
+}
